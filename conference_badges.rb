90a6321a012e3a59d9aca8f80215e771eb2283dd
Mark Bihag <markbihag330@gmail.com>
2020-01-02 20:49:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each {|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  count = 1
+  array.each do |name|
+    return "Hello, #{name}! You'll be assigned to room #{count}!"
+    count += 1 
+  end
+end
